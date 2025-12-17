17dce9f9830444e2b40cbb19586e1624267984aa
krishgokul409-ship-it <krishgokul409@gmail.com>
2025-12-17 11:44:47 +0530

    feat:variable value

---
+++ dum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class dum
+{
+    public static void main(String[] arg)
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("dummy");
+        int a=sc.nextInt();
+        System.out.println(a);
+
+    }
+}
